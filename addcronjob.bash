06f57b74b40252ae9a34124b4f547eeed2e3c867
Dylanve151 <50217470+Dylanve151@users.noreply.github.com>
2021-01-15 13:35:54 +0100

    Delete addcronjob.bash

--- addcronjob.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#crontab time
-
-if [ -z "$CRONTIME" ]
-then
-	echo "No Crontime" >> log.log
-else
-	echo "$CRONTIME root /root/wakeup.bash >> /root/log.log"$'\n' > /etc/cron.d/wakeup
-fi
